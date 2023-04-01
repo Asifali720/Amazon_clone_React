c3d079eeeb7731eafee94272aaa8fff844f1f1a5
Asif Ali <asiflashari720@gmail.com>
2023-04-01 15:46:12 +0500

    middle footer

--- src/components/FooterMiddle.js
+++ src/components/FooterMiddle.js
@@ -4,90 +4,85 @@ import FooterMiddleList from './FooterMiddleList'
 const FooterMiddle = () => {
   const arr = [
     {
-    _id:400,
-    title: 'Get to Know Us',
-    listItems: [{
-    _id:'001',
-    listData:[
-    {  
-    items: 'Careers',
-    items: 'Blog',
-    items: 'About Amazon',
-    items: 'Investor Relations',
-    items: 'Amazon Devices',
-    items: 'Amazon Science'
+      _id: 400,
+      title: 'Get to Know Us',
+      listItems: [{
+        _id: '001',
+        listData: [
+          'Careers',
+          'Blog',
+          'About Amazon',
+          'Investor Relations',
+          'Amazon Devices',
+          'Amazon Science'
+        ],
+      },
+      ],
     },
-  ],
-  },
-  ],
-  },
-  {
-  _id:401,  
-  title: 'Make Money with Us',
-  listItems:[
-  {
-  _id:'002',
-  listData: [{
-  items: 'Sell products on Amazon',
-  items: 'Sell on Amazon Business',
-  items: 'Sell apps on Amazon',
-  items: 'Become an Affiliate',
-  items: 'Advertise Your Products',
-  items: 'Self-Publish with Us',
-  items: 'Host an Amazon Hub',
-  items: '›See More Make Money with Us',
-  },
-],
-  },
-],
-},
-{
-_id:402,  
-title: 'Amazon Payment Products',
-listItems:[{
-_id:'003',
-listData:[{
-items: 'Amazon Business Card',
-items: 'Shop with Points',
-items: 'Reload Your Balance',
-items: 'Amazon Currency Converter'
-},
-],
-},
-],
-},
-{
-_id:403,  
-title: 'Let Us Help You',
-listItems:[{
-_id:'004',
-listData:[{
-items: 'Amazon and COVID-19',
-items: 'Your Account',
-items: 'Your Orders',
-items: 'Shipping Rates & Policies',
-items: 'Returns & Replacements',
-items: 'Manage Your Content and Devices',
-items: 'Amazon Assistant',
-items: 'Help',
-},
-],
-},
-],
-}
+    {
+      _id: 401,
+      title: 'Make Money with Us',
+      listItems: [
+        {
+          _id: '002',
+          listData: [
+            'Sell products on Amazon',
+            'Sell on Amazon Business',
+            'Sell apps on Amazon',
+            'Become an Affiliate',
+            'Advertise Your Products',
+            'Self-Publish with Us',
+            'Host an Amazon Hub',
+            '›See More Make Money with Us',
+          ],
+        },
+      ],
+    },
+    {
+      _id: 402,
+      title: 'Amazon Payment Products',
+      listItems: [{
+        _id: '003',
+        listData: [
+          'Amazon Business Card',
+          'Shop with Points',
+          'Reload Your Balance',
+          'Amazon Currency Converter'
+        ],
+      },
+      ],
+    },
+    {
+      _id: 403,
+      title: 'Let Us Help You',
+      listItems: [{
+        _id: '004',
+        listData: [
+          'Amazon and COVID-19',
+          'Your Account',
+          'Your Orders',
+          'Shipping Rates & Policies',
+          'Returns & Replacements',
+          'Manage Your Content and Devices',
+          'Amazon Assistant',
+          'Help',
+        ],
+      },
+      ],
+    }
   ]
   return (
     <div className=' w-full bg-[#1e2f3e] text-white'>
       <div className=' w-full py-10 border-b-[1px] border-slate-300'>
-      <div className=' max-w-5xl mx-auto'>
-        <div className='w-full grid grid-cols-4 place-items-center items-start'>
-         {
-          arr.map((node)=>(
-           <FooterMiddleList key={node._id} title={node.title} listItems={node.listItems}/>
-          ))
-         }
+        <div className=' max-w-5xl mx-auto'>
+          <div className='w-full grid grid-cols-4 place-items-center items-start'>
+            {
+              arr.map((node) => (
+                <FooterMiddleList key={node._id} title={node.title} listItems={node.listItems} />
+              ))
+            }
+          </div>
         </div>
-      </div>
       </div>
     </div>
   )
--- src/components/FooterMiddleList.js
+++ src/components/FooterMiddleList.js
@@ -3,12 +3,12 @@ import React from 'react'
 const FooterMiddleList = ({title, listItems}) => {
   return (
     <div>
-       <h3 className=' text-lg font-semibold'>{title}</h3>
+       <h3 className=' text-[16px] font-bold mb-[14px] mt-[6px]'>{title}</h3>
         <ul>
           {
-            listItems.map((item)=>item.listData.map((data)=>{
-              <li className='footerLink'>{data}</li>
-            }))
+            listItems.map((item)=>item.listData.map((data)=>(
+              <li className='footerLink py-1'>{data}</li>
+            )))
           }
         </ul>
     </div>
